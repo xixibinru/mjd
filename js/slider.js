cfbad9947d261961e2cb8e5a3a74175f21d3b44f
zhangbinru <569488212@qq.com>
2017-03-22 19:47:06 +0800

    second commit

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,6 @@
 //src:传入存放图片的父节点（例如 ul） ul中的样式自己写 涉及到seo 不写在js中 ctrl_1 ctrl_2的css需要复制
 //time: 设定轮播图播放间隔
-function slider_(src,time) {
+function slider_(src, time) {
     var slider = src.parentNode; //最大的父节点
     var srcList = src.children;
     var len = srcList.length;
@@ -8,10 +8,10 @@ function slider_(src,time) {
 
     //给轮播图添加左右控制按钮
     var ctrl_1 = document.createElement('div');
-    ctrl_1.setAttribute('class','ctrl-1');
+    ctrl_1.setAttribute('class', 'ctrl-1');
     var str = '';
-    str +=  '<span class="prev">&lt</span>'
-    str +=  '<span class="next">&gt</span>'
+    str += '<span class="prev">&lt</span>'
+    str += '<span class="next">&gt</span>'
     ctrl_1.innerHTML = str;
     slider.appendChild(ctrl_1);
     //给轮播图添加小圆点
@@ -26,64 +26,67 @@ function slider_(src,time) {
     slider.appendChild(ctrl_2);
     var num = 0; //  用num来控制播放 打乱顺序后按num继续播放
     //向左播放一次
-    function playNext(){
+    function playNext() {
         animate(srcList[num], {left: -width});
-        num ++;
-        num = num >= len? 0: num;
+        num++;
+        num = num >= len ? 0 : num;
         srcList[num].style.left = width + 'px';
         animate(srcList[num], {left: 0});
         setsquare(num);
     }
+
     //向右播放一次
-    function playPrev(){
+    function playPrev() {
         animate(srcList[num], {left: width});
-        num --;
-        num = num < 0? len-1: num;
+        num--;
+        num = num < 0 ? len - 1 : num;
         srcList[num].style.left = -width + 'px';
         animate(srcList[num], {left: 0});
         setsquare(num);
     }
+
     var prev = ctrl_1.children[0];
     var next = ctrl_1.children[1];
     //点击左按钮轮播图向右播放
-    prev.addEventListener('click',playPrev);
+    prev.addEventListener('click', playPrev);
     //点击右按钮轮播图向左播放
-    next.addEventListener('click',playNext);
+    next.addEventListener('click', playNext);
     //使小圆点样式和轮播图一致
-    function setsquare(num){
+    function setsquare(num) {
         var i = 0;
-        for(;i<len;i++){
+        for (; i < len; i++) {
             circleList[i].className = '';
         }
         circleList[num].className = 'current';
     }
+
     //给小圆点添加播放控制和点击样式
     var circleList = ctrl_2.children;
     circleList[0].className = 'current';
     var i = 0;
-    for(;i<len;i++){
-        circleList[i].addEventListener('click',function () {
-            var clicked = + this.dataset.index; //将data数据转为number类型
-            if(clicked > num){ //从右往左播放
-                animate(srcList[num],{left:-width});
+    for (; i < len; i++) {
+        circleList[i].addEventListener('click', function () {
+            var clicked = +this.dataset.index; //将data数据转为number类型
+            if (clicked > num) { //从右往左播放
+                animate(srcList[num], {left: -width});
                 srcList[clicked].style.left = width + 'px';
-            }else if(clicked < num){ //从左往右播放
-                animate(srcList[num],{left:width});
+            } else if (clicked < num) { //从左往右播放
+                animate(srcList[num], {left: width});
                 srcList[clicked].style.left = -width + 'px';
             }
-            animate(srcList[clicked],{left:0});
+            animate(srcList[clicked], {left: 0});
             num = clicked;
             setsquare(num);
         });
     }
     var timer = null;
     //设定一个自动向左播放的定时器
-    timer = setInterval(playNext,time);
+    timer = setInterval(playNext, time);
 
-    slider.addEventListener('mouseenter',function () {
+    slider.addEventListener('mouseenter', function () {
         clearInterval(timer);
     });
-    slider.addEventListener('mouseleave',function () {
+    slider.addEventListener('mouseleave', function () {
         clearInterval(timer);
         timer = setInterval(playNext, time);
     });
@@ -92,18 +95,17 @@ function slider_(src,time) {
         clearInterval(timer);
         width = srcList[0].offsetWidth;
         srcList[num].style.left = 0;
-        var i = 1;
-        for(;i<len;i++){
-            if(i!==num){
+        var i = 0;
+        for (; i < len; i++) {
+            if (i !== num) {
                 srcList[i].style.left = width + 'px';
             }
         }
         clearTimeout(R_timer);
-        var R_timer = setTimeout(function(){
+        var R_timer = setTimeout(function () {
             clearInterval(timer);
             timer = setInterval(playNext, time);
-        },time);
-
+        }, time);
     }
 }
 
